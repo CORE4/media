f0937c28f1546dea0e619e47827b03125cf04593
Bernhard Schmitt <b.schmitt@core4.de>
2016-05-02 13:03:26 +0200

    [TASK] Add InvalidImageAdjustmentConfiguration exception

---
+++ Classes/TYPO3/Media/Exception/InvalidImageAdjustmentConfigurationException.php
@@ -0,0 +1,18 @@
+<?php
+namespace TYPO3\Media\Exception;
+
+/*                                                              *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Media". *
+ *                                                              */
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Media\Exception;
+
+/**
+ * A TYPO3.Media exception to be thrown if the given image adjustment cannot be applied due to invalid configuration
+ *
+ * @api
+ */
+class InvalidImageAdjustmentConfigurationException extends Exception
+{
+
+}
